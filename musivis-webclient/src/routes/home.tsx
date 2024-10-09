0f6e136ba7c421c9379183902d90d6c08937a5e1
Job Savelsberg <jobsavelsberg@gmail.com>
2024-10-09 22:50:48 +0200

    Fix unused variable ts error for currentTrack in Home.tsx

--- musivis-webclient/src/routes/home.tsx
+++ musivis-webclient/src/routes/home.tsx
@@ -4,12 +4,10 @@ import TrackBrowser from "@/components/ui/track-browser";
 import { SpotifyRepository } from "@/services/spotify/spotifyRepository";
 import { SpotifyTrack } from "@/services/spotify/spotifyDTOs";
 import { useEffect, useState } from "react";
-import { useSpotifyPlayerStore } from "@/stores/spotifyPlayerStore";
 import { SpotifyPlayerService } from "@/services/spotify/spotifyPlayerService";
 
 function Home() {
     const [tracks, setTracks] = useState<SpotifyTrack[]>([]);
-    const { currentTrack } = useSpotifyPlayerStore();
 
     useEffect(() => {
         SpotifyRepository.getTopTracks().then((tracks) =>
@@ -18,7 +16,6 @@ function Home() {
     }, []);
 
     const onTrackClicked = (track: SpotifyTrack) => {
-        // setCurrentTrack(track as PlayableTrack);
         SpotifyPlayerService.play(track.uri);
     };
 
